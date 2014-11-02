391b73392cf62a4af27cfd7f8965ecc52c2c1097
Eric Wyne <ecwyne@gmail.com>
2014-11-02 14:59:14 -0800

    update

--- client/messages/messages.js
+++ client/messages/messages.js
@@ -1,3 +1,8 @@
 Template.messages.getContact = function(phone){
 	return Contacts.findOne({phone: phone});
 }
+
+Template.messages.hasReplied = function(){
+	var last = Messages.findOne({phone: this.phone}, {sort: {date: -1}});
+	return this.date < last.date;
+}
