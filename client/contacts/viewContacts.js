e9715f729efdfc08f65ab3b541c6e376f101f39a
Eric Wyne <ecwyne@gmail.com>
2015-01-25 16:40:55 -0800

    confirm message for changing preferences

--- client/contacts/viewContacts.js
+++ client/contacts/viewContacts.js
@@ -1,12 +1,33 @@
 Template.viewContacts.events({
 	'click .glyphicon-check': function(e){
-		var obj = {};
-		obj[e.currentTarget.dataset.key]= false
-		Contacts.update(this._id, {$set: obj})
+		var self = this;
+		confirm(function(){
+			var obj = {};
+			obj[e.currentTarget.dataset.key]= false
+			Contacts.update(self._id, {$set: obj})
+		})
 	},
 	'click .glyphicon-unchecked': function(e){
-		var obj = {};
-		obj[e.currentTarget.dataset.key]= true
-		Contacts.update(this._id, {$set: obj})
+		var self = this;
+		confirm(function(){
+			var obj = {};
+			obj[e.currentTarget.dataset.key]= true
+			Contacts.update(self._id, {$set: obj})
+		})
 	}
 })
+
+function confirm(callback){
+	swal({
+		title: "Are you sure?",   
+		text: "Would you like to change this messaging preferences?",   
+		type: "warning",   
+		showCancelButton: true,   
+		confirmButtonColor: "#DD6B55",   
+		confirmButtonText: "Yes",   
+		closeOnConfirm: false 
+	}, function(){
+		callback();
+		swal("Success!", "Preference changed.", "success"); 
+	});
+}
